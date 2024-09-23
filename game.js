57ca4bbd219eb85a797d372ca5e80c4b3d1908d7
NagarajV9 <nagaraj1999v@gmail.com>
2024-09-23 22:36:52 +0530

    complete the rock paper and scissors game
    play five rounds of game where the computer choice is calculated with Random Number Generator.

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ console.log("Rock, paper and scissors");
 let humanScore = 0;
 let computerScore = 0;
 
+playGame();
+
 function getComputerChoice() {
     let randomNumber = Math.random();
     let choice = "Scissors";
@@ -21,8 +23,84 @@ function getHumanChoice() {
 }
 
 function playRound(computerChoice, humanChoice) {
-    
+    if(computerChoice == 'Rock') {
+        if(humanChoice == "Scissors") {
+            printLose(computerChoice, humanChoice);
+        }
+        else if(humanChoice == 'Paper') {
+            printWin(computerChoice, humanChoice);
+        }
+        else {
+            printDraw();
+        }
+    }
+    else if(computerChoice == 'Scissors') {
+        if(humanChoice == 'Paper') {
+            printLose(computerChoice, humanChoice);
+        }
+        else if(humanChoice == 'Rock'){
+            printWin(computerChoice, humanChoice);
+        }
+        else {
+            printDraw();
+        }
+    }
+    else {
+        if(humanChoice == 'Rock') {
+            printLose(computerChoice, humanChoice);
+        }
+        else if(humanChoice == 'Scissors') {
+            printWin(computerChoice, humanChoice);
+        }
+        else {
+            printDraw();
+        }
+    }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+
+function printWin(computerChoice, humanChoice) {
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    ++humanScore;
+}
+
+function printLose(computerChoice, humanChoice) {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    ++computerScore;
+}
+
+function printDraw() {
+    console.log("It's a draw");
+}
+
+
+function playGame() {
+
+
+
+    for (round = 1; round <= 5; round++) {
+
+        console.log(`Round ${round}`);
+
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+
+        console.log(computerChoice);
+        console.log(humanChoice);
+
+        playRound(computerChoice, humanChoice);
+    }
+
+    if(humanScore > computerScore) {
+        console.log("Congratulations! You won the match");
+    }
+
+    else if(humanScore < computerScore) {
+        console.log("Better luck next time");
+    }
+
+    else {
+        console.log("It's a draw");
+    }
+
+}
